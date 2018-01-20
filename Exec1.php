5194ce51143d47db87b054851fbf7484cfabaa72
php 21 <php.21@lampadinha.cec.ime.usp.br>
2018-01-19 22:43:25 -0200

    Meu primeiro objeto

---
+++ Exec1.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class cliente
+ {
+   public $nome;
+   public $codigo;
+ }
+
+$a = new cliente();
+$b = new cliente();
+
+$a->nome   = "Rafael Cosentino";
+$a->codigo = 1;
+$b->nome   = "Jonas Hirata";
+$b->codigo = 2;
+
+echo "$a->nome \n";
+echo "$a->codigo \n";
+echo "$b->nome \n";
+echo "$b->codigo \n";
+
+
+class cartao
+{
+  public $numero;
+  public $Validade;
+}
+
+$c = new cartao();
+$d = new cartao();
+
+$c->numero = '1111111';
+$c->Validade = '01/01/2013';
